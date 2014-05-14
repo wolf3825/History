0a2e9dcf373dcb7b53680611f1749585f6f44ee8
William Wolfe <wolfe@tngaming.com>
2014-05-14 18:17:05 -0400

    Added History, Updated.
    Updated History by MarioE, current API tick is at 1.16.

---
+++ Commands/HistoryCommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria;
+using TShockAPI;
+using TShockAPI.DB;
+
+namespace History.Commands
+{
+    public class HistoryCommand : HCommand
+    {
+        private int x;
+        private int y;
+
+		public HistoryCommand(int x, int y, TSPlayer sender)
+			: base(sender)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+        public override void Execute()
+        {
+            List<Action> actions = new List<Action>();
+
+            using (QueryResult reader =
+                History.Database.QueryReader("SELECT Account, Action, Data, Time FROM History WHERE XY = @0 AND WorldID = @1",
+                (x << 16) + y, Main.worldID))
+            {
+                while (reader.Read())
+                {
+                    actions.Add(new Action
+                    {
+                        account = reader.Get<string>("Account"),
+                        action = (byte)reader.Get<int>("Action"),
+                        data = (ushort)(reader.Get<int>("Data")),
+                        time = reader.Get<int>("Time")
+                    });
+                }
+            }
+
+            actions.AddRange(from a in History.Actions
+                             where a.x == x && a.y == y
+                             select a);
+            sender.SendMessage("Tile history (" + x + ", " + y + "):", Color.Green);
+            foreach (Action a in actions)
+            {
+				sender.SendMessage(a.ToString(), Color.Yellow);
+            }
+            if (actions.Count == 0)
+            {
+				sender.SendMessage("No history available.", Color.Red);
+            }
+        }
+    }
+}
